92bcd9e4f9bc4d6ca3562219e8f49502a346f354
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2024-09-02 15:28:33 +0500

    uploaded

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log("Hi! This is first JS Lecture.");
+
+// datatype declaration
+// Premitive datatypes  / Non-Premitive datatypes
+
+
+
+// Premitive datatypes
+
+// Premitive datatype Definition: Those datatypes whose momory space in drive is immutable/fixed
+//
+// 1. String: A sequence of characters enclosed in single quotes or double quotes.- 32 byte
+// 2. Number: A numeric value that can be integer or floating-point.                4  byte
+// 3. Integer: A whole number without a decimal point.------------------------------ 8  byte
+// 4. Boolean: A value that can be either true or false.                            1  byte
+// 5. Symbol: A unique value that can be used as a key in an object.                
+// 6. NaN (Not a Number): A special value that represents an undefined or invalid mathematical operation.
+
+
+// String
+// number
+// integer
+// boolean
+// symbol
+// NAN      (not a number)
+// undefined
+
+
+// Non-Premitive datatypes
+
+// array
+// objects
+// functions
+
+
+let str = 'a';
+
+
+let num = 4;
+
+
+// store 5 in a variable x
+let a = 5;
+
+// Note: You cannot create a variable name on any keyword or reserved word.
+
+
+// JavaScript Variables
+// In a programming language, variables are used to store data values.
+
+// JavaScript uses the keywords (var, let and const) to declare variables.
+
+// An equal sign is used to assign values to variables. (Assignment Operator)
+
+// In this example, x is defined as a variable. Then, x is assigned (given) the value 6:
+
+let x;
+console.log(x);
+x = 6;
+// console.log(x); // Output: 6
+
+// creating a variable "name" and store a string "my name" in it.
+// output "my name";
+
+let name ='my name';
+console.log("3 + 5");
+
+
+
+
+// Assignment: go and explore all variables. | Definitions | Explore different Operators.
+// https://www.w3schools.com/js/js_operators.asp
